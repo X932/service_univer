61b2b55e13bde6f4829615d3da4107d0709e4e4b
X932 <yusuf_020202@mail.ru>
2022-02-08 16:10:25 +0500

    added relation one->many in users

--- src/modules/users/entities/users.entity.ts
+++ src/modules/users/entities/users.entity.ts
@@ -1,5 +1,9 @@
-import { UsersDepartmentsEntity } from '@relations-entities/users-departments.relation';
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { UsersDepartmentsEntity } from '@relations-entities/users-departments.relation';
+import { UsersGroupsEntity } from '@relations-entities/users-groups.relation';
+import { UsersRatingsEntity } from '@relations-entities/users-ratings.relation';
+import { UsersRolesEntity } from '@relations-entities/users-roles.relation';
+import { UsersSubjectsEntity } from '@relations-entities/users-subjects.relation';
 
 @Entity('users')
 export class UsersEntity {
@@ -8,6 +12,10 @@ export class UsersEntity {
     () => UsersDepartmentsEntity,
     (usersDepartments) => usersDepartments.user,
   )
+  @OneToMany(() => UsersGroupsEntity, (usersGroups) => usersGroups.user)
+  @OneToMany(() => UsersRatingsEntity, (usersRatings) => usersRatings.user)
+  @OneToMany(() => UsersRolesEntity, (usersRoles) => usersRoles.user)
+  @OneToMany(() => UsersSubjectsEntity, (usersSubjects) => usersSubjects.user)
   id: number;
 
   @Column()
@@ -15,4 +23,10 @@ export class UsersEntity {
 
   @Column()
   surname: string;
+
+  @Column()
+  nickname: string;
+
+  @Column()
+  password: string;
 }
