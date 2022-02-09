cfa9289c05ee4597587dc1b84003d3d21ab08b6d
X932 <yusuf_020202@mail.ru>
2022-02-09 09:46:30 +0500

    added junction table rights

--- src/modules/actions/entities/actions.entity.ts
+++ src/modules/actions/entities/actions.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { RolesEntity } from '@modules/roles/entities/roles.entity';
 
 @Entity('actions')
 export class ActionsEntity {
@@ -7,4 +8,7 @@ export class ActionsEntity {
 
   @Column()
   description: string;
+
+  @ManyToMany(() => RolesEntity, (roles) => roles.actions)
+  roles: RolesEntity[];
 }
