431e299258a17865464e1a4a99e2e84bfb659b77
Yusuf <66509035+X932@users.noreply.github.com>
2022-02-17 17:36:09 +0500

    added new package for service configs (#13)

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,14 @@ import {
   RmqOptions,
   Transport,
 } from '@nestjs/microservices';
+import { ServicesConfigs } from 'types-univer';
 import { AppModule } from './app.module';
 
 const microserviceOptions: RmqOptions = {
   transport: Transport.RMQ,
   options: {
-    urls: ['amqp://127.0.0.1:5672'],
-    queue: 'university_queue',
+    urls: [ServicesConfigs.UniverOptions.url],
+    queue: ServicesConfigs.UniverOptions.queue,
     queueOptions: {
       durable: false,
     },
